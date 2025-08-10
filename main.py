cb2ba47e6a6e0f741109c13d69020488c24a410e
znbhsn21 <zenabadnan10@gmail.com>
2025-08-10 15:25:13 +0500

    Done

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def read_expenses():
         print("Error: The JSON file is corrupt.")
         user_input = input("Do you want to delete the corrupt file and proceed? (yes/no): ").strip().lower()
         if user_input == 'yes':
-            os.remove(EXPENSES_FILE)
+            os.remove(file_path)
             print("The corrupt file has been deleted. Proceeding with an empty list.")
             return []
         else:
@@ -75,6 +75,36 @@ def delete_expense(expense_id):
     else:
         print("There is nothing to delete")
 
+def list_all():
+    if os.path.exists(file_path):
+        expenses = read_expenses()
+    else:
+        expenses = []
+    print('Here is the list of all tasks: ')
+    for expense in expenses:
+        print(expense)   
+        print()
+
+def summary_all():
+    if os.path.exists(file_path):
+        expenses = read_expenses()
+    else:
+        expenses = []
+
+    total = sum(expense["amount"] for expense in expenses)
+    print(f"Total expenses: ${total}")
+
+def summary_month(month = None):
+    if os.path.exists(file_path):
+        expenses = read_expenses()
+    else:
+        expenses = []
+
+    if month:
+        summary = [expense for expense in expenses if datetime.fromisoformat(expense['date']).month == month]
+        total = sum(expense["amount"] for expense in summary)
+        print(f"Expense for {get_month(month)}: {total}")
+
 parser = argparse.ArgumentParser(description="Expense Tracker CLI")
 subparsers = parser.add_subparsers(dest="command", help="Sub-commands")
 
@@ -85,6 +115,13 @@ parser_add.add_argument("--amount", required=True, type=float, help="Amount of e
 parser_delete = subparsers.add_parser("delete", help="Delete an expense")
 parser_delete.add_argument("--id", required=True, type=int, help="ID of the expense to delete")
 
+parser_list = subparsers.add_parser("list", help="List all expenses")
+
+parser_summary = subparsers.add_parser("summary", help ="Summary of all expenses")
+
+parser_get = subparsers.add_parser("get", help="Summary of required month")
+parser_get.add_argument("--month", type=int, choices=range(1, 13), help="Month (1-12) to filter expenses")
+
 args = parser.parse_args()
 
 match args.command:
@@ -92,6 +129,13 @@ match args.command:
         add_expenses(args.description, args.amount)
     case "delete":
         delete_expense(args.id)
+    case "list":
+        list_all()
+    case "summary":
+        summary_all()
+    case "get":
+        summary_month(args.month)
+
 
 
 
